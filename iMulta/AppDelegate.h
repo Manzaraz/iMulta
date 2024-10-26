de7fb64ae5370cb79e4f64c7931c232356e3737b
Christian Manzaraz <magnusmanz@gmail.com>
2024-10-25 21:16:52 -0300

    Initial Commit

---
+++ iMulta/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iMulta
+//
+//  Created by Christian Manzaraz on 25/10/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iMulta/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iMulta
+//
+//  Created by Christian Manzaraz on 25/10/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
